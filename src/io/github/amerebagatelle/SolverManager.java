fa0d5b26925f166e8dd1965f37719783f2ee346a
AMereBagatelle <themerebagatelle@gmail.com>
2020-12-03 07:11:08 -0500

    Day 3 Trees

--- src/io/github/amerebagatelle/SolverManager.java
+++ src/io/github/amerebagatelle/SolverManager.java
@@ -3,6 +3,7 @@ package io.github.amerebagatelle;
 import io.github.amerebagatelle.solvers.AbstractSolver;
 import io.github.amerebagatelle.solvers.Day1;
 import io.github.amerebagatelle.solvers.Day2;
+import io.github.amerebagatelle.solvers.Day3;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ public class SolverManager {
     public SolverManager() {
         solvers.add(new Day1());
         solvers.add(new Day2());
+        solvers.add(new Day3());
     }
 
     public void solve(int day, int part) {
---
+++ src/io/github/amerebagatelle/solvers/Day3.java
@@ -0,0 +1,58 @@
+package io.github.amerebagatelle.solvers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day3 extends AbstractSolver {
+    public long height;
+    public List<String> lines = new ArrayList<>();
+
+    @Override
+    public void retrieveData() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("input3.txt")));
+
+            lines.addAll(reader.lines().collect(Collectors.toList()));
+            height = reader.lines().count();
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void part1() {
+        System.out.println(getTreesForSlope(3, 1));
+    }
+
+    @Override
+    public void part2() {
+        System.out.println(getTreesForSlope(1, 1) * getTreesForSlope(3, 1) * getTreesForSlope(5, 1) * getTreesForSlope(7, 1) * getTreesForSlope(1, 2));
+    }
+
+    @Override
+    public int getDay() {
+        return 3;
+    }
+
+    private long getTreesForSlope(int right, int down) {
+        long trees = 0;
+        int x = 0;
+        for (int i = 0; i < lines.size(); i += down) {
+            if (checkLine(lines.get(i), x)) trees++;
+            x += right;
+        }
+        return trees;
+    }
+
+    private boolean checkLine(String line, int x) {
+        int xModulo = x % line.length();
+        return line.charAt(xModulo) == '#';
+    }
+}
