ea6d914cfc8e4662db8b3f036d4f3689c22b28dd
AMereBagatelle <themerebagatelle@gmail.com>
2020-12-04 19:09:59 -0500

    while(true) go brrrrrr

--- src/io/github/amerebagatelle/Main.java
+++ src/io/github/amerebagatelle/Main.java
@@ -11,13 +11,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        do {
+        while (true) {
             System.out.println("What day would you like to solve?");
             day = in.nextInt();
             System.out.println("What part of the day would you like to solve?");
             part = in.nextInt();
             solverManager.solve(day, part);
             System.out.println("Solve another?  True or False");
-        } while (in.nextBoolean());
+        }
     }
 }
